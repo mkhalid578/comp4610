a744defce63f0a9b815b5a31a01e9b4237db8a51
mkhalid578 <muhammed_khalid@student.uml.edu>
2017-12-13 12:29:07 -0500

    cleaning up directory

--- final/razor.py
+++
@@ -1,6 +0,0 @@
-import textrazor
-
-textrazor.api_key = "580f44d0d0ad4843de47e5675c44d35dd683c85c3368296d75402397"
-
-client = textrazor.TextRazor(extractors=["computer", "graphics"])
-response = client.analyze_url("problem, solving, technology")
